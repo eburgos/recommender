fc3a68ff58fdbdd68893762456d82bd99d4517fb
Eduardo Burgos <eburgos@gmail.com>
2014-06-06 11:29:34 -0400

    ...

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -14,25 +14,30 @@ namespace Test
 		[SetUp]
 		public void Setup()
 		{
-			var serializer = Newtonsoft.Json.JsonSerializer.Create();
-			using (var treader = new System.IO.StringReader(Resource1.testData1)) 
+			var serializer = Newtonsoft.Json.JsonSerializer.Create(new Newtonsoft.Json.JsonSerializerSettings());
+			using (var sreader = new System.IO.StreamReader("testData1.json")) {
+				using (var reader = new Newtonsoft.Json.JsonTextReader(sreader)) {
+					resources = serializer.Deserialize<List<Resource>>(reader);
+				}
+			}
+			/*using (var treader = new System.IO.StringReader(Resource1.testData1)) 
 			{
 				using (var reader = new Newtonsoft.Json.JsonTextReader(treader)) 
 				{
 					resources = serializer.Deserialize<List<Resource>>(reader);
 				}
-			}
+			}*/
 			space = new VectorSpace(resources);
 		}
 
-		[TestCase(TestName = "Should recommend")]
+		[Test(Description = "Should recommend")]
 		public void TestMethod1()
 		{
 			var resultSet = space.ComputeRecommendations(1);
 			var expected = resultSet.First(r => r.Uri == "http://google.com/").Result.First();
 			Assert.AreEqual("http://yahoo.com/", expected.Item2);
 		}
-		[TestCase(TestName = "Should recommend 2")]
+		[Test(Description = "Should recommend 2")]
 		public void TestMethod2()
 		{
 			var resultSet = space.ComputeRecommendations(3);
